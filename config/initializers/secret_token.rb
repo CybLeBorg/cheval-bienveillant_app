75a2efe5c78698c0f4ad7154048a8f993be4c73f
CybLeBorg <remy.leynaud@gmail.com>
2013-04-27 20:33:38 +0200

    Depot initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChevalBienveillantApp::Application.config.secret_token = 'acd12448cea5a50d22f6d410013ef4be54f0fdd4ba6af4ee217becfcb0bf9c727419f55c1aa6bbb700fb6aea563114853630916e37f69e7ce15910fb03f89ca0'
