d3a0c8048f41260396f472cfc4bae460af7967a7
CybLeBorg <remy.leynaud@gmail.com>
2013-04-28 01:16:02 +0200

    Change Gemfile

--- vendor/bundle/gems/nokogiri-1.5.9/ext/nokogiri/siteconf20130427-2563-1hgwq6e.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20130427-2563-uds4d1"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
