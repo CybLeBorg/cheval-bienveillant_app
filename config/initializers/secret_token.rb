0cb3d7bba8dd3df9793aade55f84bf9b9dc4617a
CybLeBorg <remy.leynaud@gmail.com>
2013-05-02 00:57:25 +0200

    change project structure

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ChevalBienveillantApp::Application.config.secret_token = 'acd12448cea5a50d22f6d410013ef4be54f0fdd4ba6af4ee217becfcb0bf9c727419f55c1aa6bbb700fb6aea563114853630916e37f69e7ce15910fb03f89ca0'
+ChevalBienveillantApp::Application.config.secret_token = 'a70e68064c6aa8acc193bf05864cb2216d3e6d60da4f1a2e5304a3875baa5ab81bcb26529922802c6791dae2f639f09ff45036e08e950cbe8539f5c75cff44d2'
