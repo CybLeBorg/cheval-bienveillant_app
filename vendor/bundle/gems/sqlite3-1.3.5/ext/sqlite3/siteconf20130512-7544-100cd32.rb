eceb3258d0a15f81929b46ca65e018229fed2148
CybLeBorg <remy.leynaud@gmail.com>
2013-05-12 19:26:56 +0200

    All delete and replace

---
+++ vendor/bundle/gems/sqlite3-1.3.5/ext/sqlite3/siteconf20130512-7544-100cd32.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130512-7544-1pr9zxc"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
